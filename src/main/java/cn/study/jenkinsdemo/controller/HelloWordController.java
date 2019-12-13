2660558c9e0a8d09f6eae1135513401b38b0ae22
刘福平 <liufp@gzsendi.cn>
2019-12-13 17:12:39 +0800

    测试

---
+++ src/main/java/cn/study/jenkinsdemo/controller/HelloWordController.java
@@ -0,0 +1,20 @@
+package cn.study.jenkinsdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author liufp
+ * @date 2019/12/13
+ */
+@RestController
+public class HelloWordController {
+    @GetMapping("/test")
+    public String hello(){
+        return "你好!";
+    }
+
+}
